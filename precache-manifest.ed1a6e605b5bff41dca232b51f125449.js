4191356a759f13e39c3d6ac5972de7f4826ceb6a
Adamwojty <adam12573@gmail.com>
2020-05-30 14:18:41 +0200

    Updates

---
+++ precache-manifest.ed1a6e605b5bff41dca232b51f125449.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2df810d79936447ca3f09d7cd6b2d2a",
+    "url": "/SNOW/index.html"
+  },
+  {
+    "revision": "3913a72ec77120af61ec",
+    "url": "/SNOW/static/css/main.72ba834d.chunk.css"
+  },
+  {
+    "revision": "686c290a88184a0af710",
+    "url": "/SNOW/static/js/2.f3003ee1.chunk.js"
+  },
+  {
+    "revision": "1d9b6afc326945718d0699fb1a471db4",
+    "url": "/SNOW/static/js/2.f3003ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3913a72ec77120af61ec",
+    "url": "/SNOW/static/js/main.42bb56d0.chunk.js"
+  },
+  {
+    "revision": "22fcc0092c6a7193d4ba",
+    "url": "/SNOW/static/js/runtime-main.d2b6c9e8.js"
+  },
+  {
+    "revision": "014e5b64ce74c6ac811e340c8c90b384",
+    "url": "/SNOW/static/media/about1.014e5b64.jpg"
+  },
+  {
+    "revision": "d79c5f434da59520509a847fc0c65a45",
+    "url": "/SNOW/static/media/about2.d79c5f43.jpg"
+  },
+  {
+    "revision": "80759de916cc70f388de3542007c8fa0",
+    "url": "/SNOW/static/media/cart.80759de9.svg"
+  },
+  {
+    "revision": "b66eba3c582fa756c155824ed7496a83",
+    "url": "/SNOW/static/media/chamonix.b66eba3c.jpg"
+  },
+  {
+    "revision": "98c75ae23a1b2a7fb264c620c23c19ce",
+    "url": "/SNOW/static/media/hakuba.98c75ae2.jpg"
+  },
+  {
+    "revision": "d2f70b8b6f1b4623e9ef4b7ad6d4cb90",
+    "url": "/SNOW/static/media/img2.d2f70b8b.jpg"
+  },
+  {
+    "revision": "bea1176b7244ef11c2f5067902c6fad2",
+    "url": "/SNOW/static/media/jacksonhole.bea1176b.jpg"
+  },
+  {
+    "revision": "9a8f49e1cc5e06f34133038bb673389f",
+    "url": "/SNOW/static/media/laax.9a8f49e1.jpg"
+  },
+  {
+    "revision": "1ea583bc33e6428422c15e668af02818",
+    "url": "/SNOW/static/media/shop_landning.1ea583bc.jpg"
+  },
+  {
+    "revision": "6b805b163be999546ec5cb96d64e0b82",
+    "url": "/SNOW/static/media/verbier.6b805b16.jpg"
+  }
+]);
