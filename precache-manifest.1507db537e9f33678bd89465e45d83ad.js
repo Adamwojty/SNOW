c449bbcf1280dadd916a276b8239628e1171d46f
Adamwojty <adam12573@gmail.com>
2020-05-30 14:20:17 +0200

    Updates

---
+++ precache-manifest.1507db537e9f33678bd89465e45d83ad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9169b231310cdae4926e39d9d70df74e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb0291aef664d96401a9",
+    "url": "/static/css/main.72ba834d.chunk.css"
+  },
+  {
+    "revision": "686c290a88184a0af710",
+    "url": "/static/js/2.f3003ee1.chunk.js"
+  },
+  {
+    "revision": "1d9b6afc326945718d0699fb1a471db4",
+    "url": "/static/js/2.f3003ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0291aef664d96401a9",
+    "url": "/static/js/main.82fe88dd.chunk.js"
+  },
+  {
+    "revision": "56240072e674a518867c",
+    "url": "/static/js/runtime-main.8339eb5f.js"
+  },
+  {
+    "revision": "014e5b64ce74c6ac811e340c8c90b384",
+    "url": "/static/media/about1.014e5b64.jpg"
+  },
+  {
+    "revision": "d79c5f434da59520509a847fc0c65a45",
+    "url": "/static/media/about2.d79c5f43.jpg"
+  },
+  {
+    "revision": "80759de916cc70f388de3542007c8fa0",
+    "url": "/static/media/cart.80759de9.svg"
+  },
+  {
+    "revision": "b66eba3c582fa756c155824ed7496a83",
+    "url": "/static/media/chamonix.b66eba3c.jpg"
+  },
+  {
+    "revision": "98c75ae23a1b2a7fb264c620c23c19ce",
+    "url": "/static/media/hakuba.98c75ae2.jpg"
+  },
+  {
+    "revision": "d2f70b8b6f1b4623e9ef4b7ad6d4cb90",
+    "url": "/static/media/img2.d2f70b8b.jpg"
+  },
+  {
+    "revision": "bea1176b7244ef11c2f5067902c6fad2",
+    "url": "/static/media/jacksonhole.bea1176b.jpg"
+  },
+  {
+    "revision": "9a8f49e1cc5e06f34133038bb673389f",
+    "url": "/static/media/laax.9a8f49e1.jpg"
+  },
+  {
+    "revision": "1ea583bc33e6428422c15e668af02818",
+    "url": "/static/media/shop_landning.1ea583bc.jpg"
+  },
+  {
+    "revision": "6b805b163be999546ec5cb96d64e0b82",
+    "url": "/static/media/verbier.6b805b16.jpg"
+  }
+]);
